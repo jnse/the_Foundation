5bf27b6148dccb1fa803dc88f7de9c089805e0e3
Jaakko Keränen <jaakko.keranen@iki.fi>
2017-11-21 10:43:35 +0200

    Added BSD-style copyright headers

--- include/lite/object.h
+++ include/lite/object.h
@@ -1,13 +1,38 @@
+/** @file lite/object.h  Object base class.
+
+@authors Copyright (c) 2017 Jaakko Keränen <jaakko.keranen@iki.fi>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+<small>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</small>
+*/
+
 #pragma once
 
 #include "lite/defs.h"
-
-LITE_DECLARE_IMPL(Object);
 
 /**
  * Reference-counted object that owns child objects and may have a parent.
  * Objects get deleted only after all references have been removed.
  */
+LITE_DECLARE_IMPL(Object);
 
 iObject *   iObject_new         (void);
 void        iObject_release     (iObject *);
