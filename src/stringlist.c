8b0062e49984c5c847dd6008d15dfd356213ce23
Jaakko Keränen <jaakko.keranen@iki.fi>
2017-12-09 23:09:10 +0200

    StringList: Updated max strings count

--- src/stringlist.c
+++ src/stringlist.c
@@ -31,7 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</small>
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define iStringListMaxStringsPerNode    64 //1024
+#define iStringListMaxStringsPerNode    1024
 
 iDeclareType(StringListNode)
 
