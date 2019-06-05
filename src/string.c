36abe7e2c5161119ca5c32e5b9450e10a009e4ef
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-06-05 17:21:39 +0300

    String: Query locale only once

--- src/string.c
+++ src/string.c
@@ -45,8 +45,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</small>
 
 static char localeCharSet_[64];
 
-static inline const char *currentLocaleLanguage_(void) {    
-    return uc_locale_language();
+static inline const char *currentLocaleLanguage_(void) {
+    static iString *loc = NULL;
+    if (!loc) {
+        loc = newCStr_String(uc_locale_language());
+    }
+    return cstr_String(loc);
 }
 
 iChar upper_Char(iChar d) {
