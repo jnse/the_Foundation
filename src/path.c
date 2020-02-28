6df2bcbff339db9f0a0b9e4246323c39e24040ca
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-02-28 10:38:38 +0200

    Cleanup
    Isolate libunistring usage to just string.c.

--- src/path.c
+++ src/path.c
@@ -29,7 +29,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</small>
 #include "the_Foundation/string.h"
 
 #include <unistd.h>
-#include <unicase.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
@@ -80,7 +79,7 @@ iBool isAbsolute_Path(const iString *d) {
     if (size_String(d) >= 3) {
         iStringConstIterator i;
         init_StringConstIterator(&i, d);
-        const iChar drive = uc_toupper(i.value);
+        const iChar drive = upper_Char(i.value);
         if (drive < 'A' || drive > 'Z') {
             return iFalse;
         }
