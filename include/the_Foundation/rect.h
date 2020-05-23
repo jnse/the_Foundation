babfe8e068911cfb331dd5b5218f0ff551d43e96
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-05-23 21:16:20 +0300

    Rect: Added area() method

--- include/the_Foundation/rect.h
+++ include/the_Foundation/rect.h
@@ -69,6 +69,7 @@ iLocalDef int   top_Rect    (const iRect d) { return d.pos.y; }
 iLocalDef int   bottom_Rect (const iRect d) { return d.pos.y + d.size.y; }
 iLocalDef int   width_Rect  (const iRect d) { return d.size.x; }
 iLocalDef int   height_Rect (const iRect d) { return d.size.y; }
+iLocalDef int   area_Rect   (const iRect d) { return d.size.x * d.size.y; }
 iLocalDef iInt2 mid_Rect    (const iRect d) { return add_I2(d.pos, divi_I2(d.size, 2)); }
 
 iLocalDef iInt2 topLeft_Rect(const iRect d) { return d.pos; }
